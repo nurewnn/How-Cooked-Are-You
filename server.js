cc47b9c1973a371995c4fce9f21e56ef91bacbfa
aniss <anissophea12@gmail.com>
2026-08-04 22:55:30 +0800

    refactor: extract FuzzyEngine and update local API routing

--- server.js
+++ server.js
@@ -139,6 +139,17 @@ function callGemini(prompt, systemInstruction) {
 
 // Server requests dispatcher
 const server = http.createServer((req, res) => {
+    // Enable CORS
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+    if (req.method === 'OPTIONS') {
+        res.writeHead(204);
+        res.end();
+        return;
+    }
+
     const url = req.url;
     const method = req.method;
 
